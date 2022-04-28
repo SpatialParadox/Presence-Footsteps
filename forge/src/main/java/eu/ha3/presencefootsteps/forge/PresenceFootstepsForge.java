168bf7af5e673b45b0b540461ac324d208b2ed5d
SpatialParadox <notparadox04@gmail.com>
2022-04-28 20:11:37 +0100

    forge: ignore server version

--- forge/src/main/java/eu/ha3/presencefootsteps/forge/PresenceFootstepsForge.java
+++ forge/src/main/java/eu/ha3/presencefootsteps/forge/PresenceFootstepsForge.java
@@ -3,13 +3,20 @@ package eu.ha3.presencefootsteps.forge;
 import dev.architectury.platform.forge.EventBuses;
 import eu.ha3.presencefootsteps.PresenceFootsteps;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(PresenceFootsteps.MOD_ID)
 public class PresenceFootstepsForge {
     public PresenceFootstepsForge() {
+        // Ignore server version of mod
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         EventBuses.registerModEventBus(PresenceFootsteps.MOD_ID, eventBus);
 
